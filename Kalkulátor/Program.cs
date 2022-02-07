cb21a52d263bc58a035ada20ffbd75ba0438644b
Ákos Szabadi <2021301@dszcberegszaszi.hu>
2022-02-07 13:22:32 +0100

    ...

---
+++ Kalkulátor/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalkulátor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int szam1;
+            int szam2;
+            char muvjel;
+            float ered = 0;
+            string uzen = "A művelet eredménye: ";
+
+
+            szam1 = int.Parse(Console.ReadLine());
+            szam2 = int.Parse(Console.ReadLine());
+            muvjel = Convert.ToChar(Console.Read());
+
+            switch (muvjel)
+            {
+                case '+':
+                    ered = szam1 + szam2;
+                    break;
+
+                case '-':
+                    ered = szam1 - szam2;
+                    break;
+
+                case '*':
+                    ered = szam1 * szam2;
+                    break;
+
+                case '/':
+                    ered = szam1 / szam2;
+                    break;
+
+                default:
+                    uzen = "Hiba";
+                    break;
+            }
+            Console.WriteLine(uzen + ered);
+            Console.ReadKey();
+        }
+    }
+}
