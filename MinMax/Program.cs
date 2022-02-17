88b89139dde2cc1a47739d7a030eea8b4f62b29d
Ákos Szabadi <2021301@dszcberegszaszi.hu>
2022-02-17 09:42:03 +0100

    Órai munka

---
+++ MinMax/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int min = 1000;
+            int max = -1;
+            int minIdx = 0;
+            int maxIdx = 0;
+
+            Random rnd = new Random();
+            int[] arr = new int[100];
+
+            for (int i = 0; i < arr.Length; ++i)
+            {
+                arr[i] = rnd.Next(max,min);
+            }
+            for (int i = 0; i < 30; ++i) 
+            {
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                    minIdx = i;
+                }
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                    maxIdx = i;
+                }
+
+            }
+            Console.WriteLine("Minimum {0}, index {1}", min, minIdx);
+            Console.WriteLine("Maximum {0}, index {1}", max, maxIdx);
+            Console.ReadKey();
+        }
+    }
+}
