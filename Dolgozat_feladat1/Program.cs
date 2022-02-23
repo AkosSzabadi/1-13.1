52ce824e3c35595344eb118082ff5b564f1a55ad
Ákos Szabadi <2021301@dszcberegszaszi.hu>
2022-02-23 09:13:18 +0100

    Dolgozat1

---
+++ Dolgozat_feladat1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dolgozat_feladat1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string elsoszam;
+            string masodikszam;
+            int a;
+            int b;
+
+            Console.WriteLine("Adj meg egy számot! ");
+            elsoszam = Console.ReadLine();
+            a = Convert.ToInt32(elsoszam);
+
+            Console.WriteLine("Add meg a második számot! ");
+            masodikszam = Console.ReadLine();
+            b = Convert.ToInt32(masodikszam);
+
+            if (a > 0 && b > 0)
+                Console.WriteLine("Mind a két szám pozitív");
+            if (a > 0 && b < 0)
+                Console.WriteLine("Az első szám pozitív");
+            if (a < 0 && b > 0)
+                Console.WriteLine("A második szám pozitív");
+            if (a < 0 && b < 0)
+                Console.WriteLine("Mind a két szám negatív");
+
+            Console.ReadKey();
+        }
+    }
+}
