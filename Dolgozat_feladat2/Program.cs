abc20a4c8382e771d817f03bb22c1c48fd2bfda1
Ákos Szabadi <2021301@dszcberegszaszi.hu>
2022-02-23 10:16:26 +0100

    Dolgozat2

---
+++ Dolgozat_feladat2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dolgozat_feladat2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cim;
+            string oldalak;
+            int a;
+            int i = 0;
+
+            Console.WriteLine("Add meg a könyv címét: ");
+            Console.ReadLine();
+
+            Console.WriteLine("Add meg az oldalainak a számát: ");
+            oldalak = Console.ReadLine();
+            a = Convert.ToInt32(oldalak);
+
+            if (a > 150)
+                Console.WriteLine("A könyv hosszú terjedelmű.");
+            else 
+                Console.WriteLine("A könyv rövid terjedelmű.");
+
+            Console.ReadKey();    
+        }
+    }
+}
---
+++ Dolgozat_feladat3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dolgozat_feladat3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            var nevek = new string[]
+            {
+                "Kiss Pista","Piros Anna","Nagy Lajos"
+            };
+            var neveredmeny = new string[3];
+
+            var foglalkozas = new string[]
+            {
+                "kovács","informatikus","sofőr"
+            };
+            var fogeredmeny = new string[3];
+
+
+
+
+            Console.WriteLine("Add meg a nevet: ");
+            Console.ReadLine();
+
+            Console.WriteLine("Add meg a foglalkozását: ");
+            Console.ReadLine();
+
+            Console.WriteLine("Add meg a nemét: (f/n) ");
+            Console.ReadLine();
+        }
+    }
+}
