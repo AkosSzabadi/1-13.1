1aa1dbaf279953ec9c9215241c03fc9a2712612f
Ákos Szabadi <2021301@dszcberegszaszi.hu>
2022-02-23 11:28:34 +0100

    Órai munka

---
+++ Islands/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Islands
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int isLandCount = 0
+        }
+    }
+}
