a77361c64b77b12fa397dc7dbf3a2b7119a5d347
Ákos Szabadi <2021301@dszcberegszaszi.hu>
2022-03-07 14:25:08 +0100

    Órai munka

---
+++ Vektor/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vektor
+{
+    class Vektor
+    {
+        private double hossz;
+        private int elemszam;
+        private int[] tomb = new int[]{ };
+
+
+        public Vektor(double hossz, int elemszam) 
+        {
+            this.hossz = hossz;
+            this.elemszam = elemszam;
+        }
+
+        Random fel = new Random();
+        public Vektor() {}
+
+        public void feltoltes(int elemszam, int[]tomb)
+        {
+            Console.WriteLine("Vektor hossza: ");
+            elemszam = int.Parse(Console.ReadLine());
+            for (int i = 0; i<=elemszam; i++)
+            {
+                tomb[i] = fel.Next();
+            }
+        }
+        public double getTomb()
+        {
+            return this.elemszam;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            Console.ReadKey();
+        }
+    }
+}
