ea7315c29b3be8ccd7407cc4cd09dab38ab939cd
Ákos Szabadi <2021301@dszcberegszaszi.hu>
2022-03-28 10:50:45 +0200

    órai munka

---
+++ Órai/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Órai
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int szam;
+                do
+                { 
+                Console.WriteLine("Give me a number: ");
+                szam = int.Parse(Console.ReadLine());
+                try
+                {
+                    int szam = int.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("");
+                }
+            }
+        }
+    }
+}
