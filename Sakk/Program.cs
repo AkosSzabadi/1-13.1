528def932470c207c0eca403aece227c3ec82c14
Ákos Szabadi <2021301@dszcberegszaszi.hu>
2022-04-05 12:16:42 +0200

    Sakk feladat

---
+++ Sakk/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sakk
+{
+    class Bastya
+    {
+        private int
+            bastya1,
+            bastya2,
+            bastya3,
+            bastya4,
+            bastya5,
+            bastya6,
+            bastya7,
+            bastya8;
+        int[,] table = new int[8, 8];
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
