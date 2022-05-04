08fc46c2b4232b61e43bee185fccb663fa8f6de9
Ákos Szabadi <2021301@dszcberegszaszi.hu>
2022-05-04 14:53:56 +0200

    Órai munka

---
+++ Teszt/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teszt
+{
+    class Oszthato
+    {
+        private int beker;
+        private int ertek;
+
+        public Oszthato(int beker)
+        {
+            this.ertek = beker;
+        }
+        public void setSzam()
+        {
+            try
+            {
+                this.ertek = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+        }
+        public void getSzam()
+        {
+            if(this.ertek % 3 == 0)
+            {
+                Console.WriteLine("Három");
+            }
+            else
+            {
+                if(this.ertek % 5 == 0)
+                {
+                    Console.WriteLine("Öt");
+                }
+            }
+            else
+            {
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
