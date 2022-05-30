e1aad71afcbfce6847df9f5bdcdac76f719e31ce
Ákos Szabadi <2021301@dszcberegszaszi.hu>
2022-05-30 13:02:06 +0200

    Órai

---
+++ FormMozg/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿
+namespace FormMozg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnKozep = new System.Windows.Forms.Button();
+            this.btnFel = new System.Windows.Forms.Button();
+            this.btnFelul = new System.Windows.Forms.Button();
+            this.btnBalra = new System.Windows.Forms.Button();
+            this.btnBalszel = new System.Windows.Forms.Button();
+            this.btnJobbra = new System.Windows.Forms.Button();
+            this.btnJobbszel = new System.Windows.Forms.Button();
+            this.btnLefele = new System.Windows.Forms.Button();
+            this.btnAlul = new System.Windows.Forms.Button();
+            this.gpbAtlatszo = new System.Windows.Forms.GroupBox();
+            this.btnOpNo = new System.Windows.Forms.Button();
+            this.btnOpCsok = new System.Windows.Forms.Button();
+            this.gpbMeret = new System.Windows.Forms.GroupBox();
+            this.btnMeretNo = new System.Windows.Forms.Button();
+            this.btnMeretCsok = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.gpbAtlatszo.SuspendLayout();
+            this.gpbMeret.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnKozep
+            // 
+            this.btnKozep.Location = new System.Drawing.Point(343, 223);
+            this.btnKozep.Name = "btnKozep";
+            this.btnKozep.Size = new System.Drawing.Size(75, 23);
+            this.btnKozep.TabIndex = 0;
+            this.btnKozep.Text = "Középre";
+            this.btnKozep.UseVisualStyleBackColor = true;
+            // 
+            // btnFel
+            // 
+            this.btnFel.Location = new System.Drawing.Point(343, 182);
+            this.btnFel.Name = "btnFel";
+            this.btnFel.Size = new System.Drawing.Size(75, 23);
+            this.btnFel.TabIndex = 0;
+            this.btnFel.Text = "Fel";
+            this.btnFel.UseVisualStyleBackColor = true;
+            // 
+            // btnFelul
+            // 
+            this.btnFelul.Location = new System.Drawing.Point(343, 133);
+            this.btnFelul.Name = "btnFelul";
+            this.btnFelul.Size = new System.Drawing.Size(75, 23);
+            this.btnFelul.TabIndex = 0;
+            this.btnFelul.Text = "Felülre";
+            this.btnFelul.UseVisualStyleBackColor = true;
+            // 
+            // btnBalra
+            // 
+            this.btnBalra.Location = new System.Drawing.Point(243, 223);
+            this.btnBalra.Name = "btnBalra";
+            this.btnBalra.Size = new System.Drawing.Size(75, 23);
+            this.btnBalra.TabIndex = 0;
+            this.btnBalra.Text = "Balra";
+            this.btnBalra.UseVisualStyleBackColor = true;
+            // 
+            // btnBalszel
+            // 
+            this.btnBalszel.Location = new System.Drawing.Point(141, 223);
+            this.btnBalszel.Name = "btnBalszel";
+            this.btnBalszel.Size = new System.Drawing.Size(75, 23);
+            this.btnBalszel.TabIndex = 0;
+            this.btnBalszel.Text = "Balszélre";
+            this.btnBalszel.UseVisualStyleBackColor = true;
+            // 
+            // btnJobbra
+            // 
+            this.btnJobbra.Location = new System.Drawing.Point(436, 223);
+            this.btnJobbra.Name = "btnJobbra";
+            this.btnJobbra.Size = new System.Drawing.Size(75, 23);
+            this.btnJobbra.TabIndex = 0;
+            this.btnJobbra.Text = "Jobbra";
+            this.btnJobbra.UseVisualStyleBackColor = true;
+            this.btnJobbra.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // btnJobbszel
+            // 
+            this.btnJobbszel.Location = new System.Drawing.Point(537, 223);
+            this.btnJobbszel.Name = "btnJobbszel";
+            this.btnJobbszel.Size = new System.Drawing.Size(75, 23);
+            this.btnJobbszel.TabIndex = 0;
+            this.btnJobbszel.Text = "Jobbszélre";
+            this.btnJobbszel.UseVisualStyleBackColor = true;
+            // 
+            // btnLefele
+            // 
+            this.btnLefele.Location = new System.Drawing.Point(343, 268);
+            this.btnLefele.Name = "btnLefele";
+            this.btnLefele.Size = new System.Drawing.Size(75, 23);
+            this.btnLefele.TabIndex = 0;
+            this.btnLefele.Text = "Lefele";
+            this.btnLefele.UseVisualStyleBackColor = true;
+            // 
+            // btnAlul
+            // 
+            this.btnAlul.Location = new System.Drawing.Point(343, 311);
+            this.btnAlul.Name = "btnAlul";
+            this.btnAlul.Size = new System.Drawing.Size(75, 23);
+            this.btnAlul.TabIndex = 0;
+            this.btnAlul.Text = "Alulra";
+            this.btnAlul.UseVisualStyleBackColor = true;
+            // 
+            // gpbAtlatszo
+            // 
+            this.gpbAtlatszo.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.gpbAtlatszo.Controls.Add(this.label2);
+            this.gpbAtlatszo.Controls.Add(this.btnOpNo);
+            this.gpbAtlatszo.Location = new System.Drawing.Point(141, 363);
+            this.gpbAtlatszo.Name = "gpbAtlatszo";
+            this.gpbAtlatszo.Size = new System.Drawing.Size(508, 77);
+            this.gpbAtlatszo.TabIndex = 1;
+            this.gpbAtlatszo.TabStop = false;
+            // 
+            // btnOpNo
+            // 
+            this.btnOpNo.Location = new System.Drawing.Point(174, 30);
+            this.btnOpNo.Name = "btnOpNo";
+            this.btnOpNo.Size = new System.Drawing.Size(75, 23);
+            this.btnOpNo.TabIndex = 0;
+            this.btnOpNo.Text = "Növekszik";
+            this.btnOpNo.UseVisualStyleBackColor = true;
+            // 
+            // btnOpCsok
+            // 
+            this.btnOpCsok.Location = new System.Drawing.Point(409, 393);
+            this.btnOpCsok.Name = "btnOpCsok";
+            this.btnOpCsok.Size = new System.Drawing.Size(75, 23);
+            this.btnOpCsok.TabIndex = 0;
+            this.btnOpCsok.Text = "Csökken";
+            this.btnOpCsok.UseVisualStyleBackColor = true;
+            // 
+            // gpbMeret
+            // 
+            this.gpbMeret.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.gpbMeret.Controls.Add(this.label1);
+            this.gpbMeret.Controls.Add(this.btnMeretCsok);
+            this.gpbMeret.Controls.Add(this.btnMeretNo);
+            this.gpbMeret.Location = new System.Drawing.Point(141, 12);
+            this.gpbMeret.Name = "gpbMeret";
+            this.gpbMeret.Size = new System.Drawing.Size(508, 77);
+            this.gpbMeret.TabIndex = 1;
+            this.gpbMeret.TabStop = false;
+            // 
+            // btnMeretNo
+            // 
+            this.btnMeretNo.Location = new System.Drawing.Point(174, 30);
+            this.btnMeretNo.Name = "btnMeretNo";
+            this.btnMeretNo.Size = new System.Drawing.Size(75, 23);
+            this.btnMeretNo.TabIndex = 0;
+            this.btnMeretNo.Text = "Növekszik";
+            this.btnMeretNo.UseVisualStyleBackColor = true;
+            // 
+            // btnMeretCsok
+            // 
+            this.btnMeretCsok.Location = new System.Drawing.Point(268, 30);
+            this.btnMeretCsok.Name = "btnMeretCsok";
+            this.btnMeretCsok.Size = new System.Drawing.Size(75, 23);
+            this.btnMeretCsok.TabIndex = 0;
+            this.btnMeretCsok.Text = "Csökken";
+            this.btnMeretCsok.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(6, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Méret";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(6, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Átlátszóság";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(788, 463);
+            this.Controls.Add(this.btnOpCsok);
+            this.Controls.Add(this.gpbMeret);
+            this.Controls.Add(this.gpbAtlatszo);
+            this.Controls.Add(this.btnBalszel);
+            this.Controls.Add(this.btnBalra);
+            this.Controls.Add(this.btnFelul);
+            this.Controls.Add(this.btnFel);
+            this.Controls.Add(this.btnJobbszel);
+            this.Controls.Add(this.btnJobbra);
+            this.Controls.Add(this.btnAlul);
+            this.Controls.Add(this.btnLefele);
+            this.Controls.Add(this.btnKozep);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.gpbAtlatszo.ResumeLayout(false);
+            this.gpbAtlatszo.PerformLayout();
+            this.gpbMeret.ResumeLayout(false);
+            this.gpbMeret.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnKozep;
+        private System.Windows.Forms.Button btnFel;
+        private System.Windows.Forms.Button btnFelul;
+        private System.Windows.Forms.Button btnBalra;
+        private System.Windows.Forms.Button btnBalszel;
+        private System.Windows.Forms.Button btnJobbra;
+        private System.Windows.Forms.Button btnJobbszel;
+        private System.Windows.Forms.Button btnLefele;
+        private System.Windows.Forms.Button btnAlul;
+        private System.Windows.Forms.GroupBox gpbAtlatszo;
+        private System.Windows.Forms.Button btnOpNo;
+        private System.Windows.Forms.Button btnOpCsok;
+        private System.Windows.Forms.GroupBox gpbMeret;
+        private System.Windows.Forms.Button btnMeretCsok;
+        private System.Windows.Forms.Button btnMeretNo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
